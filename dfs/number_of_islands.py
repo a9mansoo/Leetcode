fc2678a7b867b4f77c66dbedbfe4e25924fe1486
a9mansoo <a9mansoo@uwaterloo.ca>
2026-06-28 18:14:54 -0400

    Add recursive solution

--- dfs/number_of_islands.py
+++ dfs/number_of_islands.py
@@ -43,7 +43,44 @@ def numberOfIslands(grid):
 
 
             
+def numberOfIslandsRecursive(grid):
+    visited = []
 
+    def dfs(square):
+        i = square[0]
+        j = square[1]
+
+        if (i, j) in visited:
+            return
+        
+        visited.append((i, j))
+
+        # Now look for neighbours which are 1
+        # Up
+        if (i - 1) >= 0 and grid[i-1][j] == "1":
+            dfs((i-1, j))
+
+        # Down
+        if (i+1) < len(grid) and grid[i+1][j] == "1":
+            dfs((i+1, j))
+
+        # Left
+        if (j - 1) >= 0 and grid[i][j-1] == "1":
+            dfs((i, j-1))
+
+        # Right
+        if (j + 1) < len(grid[0]) and grid[i][j+1] == "1":
+            dfs((i, j+1))
+
+
+
+    islands = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1" and (i, j) not in visited:
+                islands += 1
+                dfs((i, j))
 
 
 
